d0001ebe9e580d21ca6c7aaad6edd9d143b18956
Kaua676 <111838911+Kaua676@users.noreply.github.com>
2025-02-24 13:33:07 -0400

    Update Kaua.sql

--- Kaua.sql
+++ Kaua.sql
@@ -1,36 +1,36 @@
-CREATE TABLE fornecedores {
+CREATE TABLE fornecedores (
     id SERIAL PRIMARY KEY,
     nome VARCHAR(255),
     cpnj VARCHAR(18),
     telefone VARCHAR(15),
     endereco VARCHAR(255)
-};
+);
 
-CREATE TABLE clientes{
+CREATE TABLE clientes(
     id SERIAL PRIMARY KEY,
     nome VARCHAR(255),
     cpf_cnpj VARCHAR(18),
     telefone VARCHAR(15),
     endereco VARCHAR(255)
-};
+);
 
-CREATE TABLE produtos{
+CREATE TABLE produtos(
     id SERIAL PRIMARY KEY,
     nome VARCHAR(255),
     tipo VARCHAR(255),
     preco_unitario FLOAT,
     unidade_medida VARCHAR(10)
-};
+);
 
-CREATE TABLE estoque{
+CREATE TABLE estoque(
     id SERIAL PRIMARY KEY,
     quantidade INT,
     data_ultima_atualizacao DATE,
     fk_produto INT,
     CONSTRAINT fk_produto_estoque FOREIGN KEY (fk_produto) REFERENCES produtos(id) ON DELETE CASCADE
-};
+);
 
-CREATE TABLE compras{
+CREATE TABLE compras(
     id SERIAL PRIMARY KEY,
     quantidade INT,
     data_compra DATE,
@@ -39,9 +39,9 @@ CREATE TABLE compras{
     fk_produto INT,
     CONSTRAINT fk_fornecedor_compras FOREIGN KEY (fk_fornecedor) REFERENCES fornecedores(id) ON DELETE CASCADE,
     CONSTRAINT fk_produto_compras FOREIGN KEY (fk_produto) REFERENCES produtos(id) ON DELETE CASCADE
-};
+);
 
-CREATE TABLE vendas{
+CREATE TABLE vendas(
     id SERIAL PRIMARY KEY,
     quantidade INT,
     data_venda DATE,
@@ -50,6 +50,6 @@ CREATE TABLE vendas{
     fk_produto INT,
     CONSTRAINT fk_cliente_vendas FOREIGN KEY (fk_cliente) REFERENCES clientes(id) ON DELETE CASCADE,
     CONSTRAINT fk_produto_vendas FOREIGN KEY (fk_produto) REFERENCES produtos(id) ON DELETE CASCADE
-};
+);
 
 
