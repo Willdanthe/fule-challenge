f84f098832b8d8782297a23bc39564ad306a47f9
Kaua676 <111838911+Kaua676@users.noreply.github.com>
2025-02-24 13:40:11 -0400

    Update Kaua.sql

--- Kaua.sql
+++ Kaua.sql
@@ -52,4 +52,50 @@ CREATE TABLE vendas(
     CONSTRAINT fk_produto_vendas FOREIGN KEY (fk_produto) REFERENCES produtos(id) ON DELETE CASCADE
 );
 
+-- Insert da tabela FORNECEDORES
+INSERT INTO fornecedores (nome, cpnj, telefone, endereco) VALUES ('Fornecedor A', '12345678901234', '1234567890', 'Rua A, 123');
+INSERT INTO fornecedores (nome, cpnj, telefone, endereco) VALUES ('Fornecedor B', '98765432109876', '9876543210', 'Rua B, 456');
+INSERT INTO fornecedores (nome, cpnj, telefone, endereco) VALUES ('Fornecedor C', '56789012345678', '8765432109', 'Rua C, 789');
+INSERT INTO forneceddores (nome, cpnj, telefone, endereco) VALUES ('Fornecedor D', '12345678901234', '1234567890', 'Rua A, 123');
+INSERT INTO fornecedores (nome, cpnj, telefone, endereco) VALUES ('Fornecedor E', '98765432109876', '9876543210', 'Rua B, 456');
+INSERT INTO fornecedores (nome, cpnj, telefone, endereco) VALUES ('Fornecedor F', '56789012345678', '8765432109', 'Rua C, 789');
 
+-- Insert da tabela CLIENTES
+INSERT INTO clientes (nome, cpf_cnpj, telefone, endereco) VALUES ('Cliente A', '12345678901', '1234567890', 'Rua A, 123');
+INSERT INTO clientes (nome, cpf_cnpj, telefone, endereco) VALUES ('Cliente B', '98765432109', '9876543210', 'Rua B, 456');
+INSERT INTO clientes (nome, cpf_cnpj, telefone, endereco) VALUES ('Cliente C', '56789012345', '8765432109', 'Rua C, 789');
+INSERT INTO clientes (nome, cpf_cnpj, telefone, endereco) VALUES ('Cliente D', '12345678901', '1234567890', 'Rua A, 123');
+INSERT INTO clientes (nome, cpf_cnpj, telefone, endereco) VALUES ('Cliente E', '98765432109', '9876543210', 'Rua B, 456');
+INSERT INTO clientes (nome, cpf_cnpj, telefone, endereco) VALUES ('Cliente F', '56789012345', '8765432109', 'Rua C, 789');
+
+-- Insert da tabela PRODUTOS
+INSERT INTO produtos (nome, tipo, preco_unitario, unidade_medida) VALUES ('Produto A', 'Tipo A', 10.00, 'Unidade');
+INSERT INTO produtos (nome, tipo, preco_unitario, unidade_medida) VALUES ('Produto B', 'Tipo B', 20.00, 'Unidade');
+INSERT INTO produtos (nome, tipo, preco_unitario, unidade_medida) VALUES ('Produto C', 'Tipo C', 30.00, 'Unidade');
+INSERT INTO produtos (nome, tipo, preco_unitario, unidade_medida) VALUES ('Produto D', 'Tipo D', 40.00, 'Unidade');
+INSERT INTO produtos (nome, tipo, preco_unitario, unidade_medida) VALUES ('Produto E', 'Tipo E', 50.00, 'Unidade');
+INSERT INTO produtos (nome, tipo, preco_unitario, unidade_medida) VALUES ('Produto F', 'Tipo F', 60.00, 'Unidade');
+
+-- Insert da tabela ESTOQUE
+INSERT INTO estoque (quantidade, data_ultima_atualizacao, fk_produto) VALUES (10, '2023-01-01', 1);
+INSERT INTO estoque (quantidade, data_ultima_atualizacao, fk_produto) VALUES (20, '2023-02-01', 2);
+INSERT INTO estoque (quantidade, data_ultima_atualizacao, fk_produto) VALUES (30, '2023-03-01', 3);
+INSERT INTO estoque (quantidade, data_ultima_atualizacao, fk_produto) VALUES (40, '2023-04-01', 4);
+INSERT INTO estoque (quantidade, data_ultima_atualizacao, fk_produto) VALUES (50, '2023-05-01', 5);
+INSERT INTO estoque (quantidade, data_ultima_atualizacao, fk_produto) VALUES (60, '2023-06-01', 6);
+
+-- Insert da tabela VENDAS
+INSERT INTO vendas (data_venda, valor_total, fk_cliente, fk_produto) VALUES ('2023-01-01', 100.00, 1, 1);
+INSERT INTO vendas (data_venda, valor_total, fk_cliente, fk_produto) VALUES ('2023-02-01', 200.00, 2, 2);
+INSERT INTO vendas (data_venda, valor_total, fk_cliente, fk_produto) VALUES ('2023-03-01', 300.00, 3, 3);
+INSERT INTO vendas (data_venda, valor_total, fk_cliente, fk_produto) VALUES ('2023-04-01', 400.00, 4, 4);
+INSERT INTO vendas (data_venda, valor_total, fk_cliente, fk_produto) VALUES ('2023-05-01', 500.00, 5, 5);
+INSERT INTO vendas (data_venda, valor_total, fk_cliente, fk_produto) VALUES ('2023-06-01', 600.00, 6, 6);
+
+-- Insert da tabela COMPRAS
+INSERT INTO compras (data_compra, valor_total, fk_fornecedor, fk_produto) VALUES ('2023-01-01', 100.00, 1, 1);
+INSERT INTO compras (data_compra, valor_total, fk_fornecedor, fk_produto) VALUES ('2023-02-01', 200.00, 2, 2);
+INSERT INTO compras (data_compra, valor_total, fk_fornecedor, fk_produto) VALUES ('2023-03-01', 300.00, 3, 3);
+INSERT INTO compras (data_compra, valor_total, fk_fornecedor, fk_produto) VALUES ('2023-04-01', 400.00, 4, 4);
+INSERT INTO compras (data_compra, valor_total, fk_fornecedor, fk_produto) VALUES ('2023-05-01', 500.00, 5, 5);
+INSERT INTO compras (data_compra, valor_total, fk_fornecedor, fk_produto) VALUES ('2023-06-01', 600.00, 6, 6);
